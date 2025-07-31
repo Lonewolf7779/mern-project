23ffdb5b8d4a86116b8bb198c34309b4c95c7a3b
Santosh <sntoshprajapati163@gmail.com>
2025-07-31 19:15:25 +0530

    Connected the MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 
-dotenv.config();
+dotenv.config(); // Must come AFTER checking path
 
 const app = express();
 
-app.get('/products',(req,res) =>
-{
-    //res.send("Server is ready123!");
+app.get("/products",(req,res)=>{});
+
+console.log("MONGO_URI:", process.env.MONGO_URI); // 👈 Check if it's still undefined
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at  http://localhost:5000 ");
 });
-
-console.log(process.env.MONGO_URI);
-
-app.listen(5000,()=>{
-console.log("Server started at  http://localhost:5000 ")
-});
-
-
-
-//xj3wMNOcNyXmB0dl
