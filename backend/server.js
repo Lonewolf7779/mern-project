d9add63e1ef5e41501aaf462ae1f05e2c94994ac
Santosh <sntoshprajapati163@gmail.com>
2025-07-31 18:45:49 +0530

    Adding the MONGO_URI

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get('/',(req,res) =>
+app.get('/products',(req,res) =>
 {
-    res.send("Server is ready!");
+    //res.send("Server is ready123!");
 });
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000,()=>{
 console.log("Server started at  http://localhost:5000 ")
 });
+
+
+
+//xj3wMNOcNyXmB0dl
