76ee96af6fe3a8a190018329e4fd9993f3754cf4
Santosh <sntoshprajapati163@gmail.com>
2025-08-02 19:33:30 +0530

    Created a Schema named product and defined parameters

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
-
+//moongoose is an ODM(Object data modelling) library for mongoDB and node js.
+//It allows you to define schemas for your MongoDB collection 
+//Schema---it is actiually a blueprint for your your document like a structure or a  contract
 const productSchema = new mongoose.Schema({
 
-    name:String,
+   name:{
+    type:String,
     required:true
+   },
+
+   price:{
+    type:String,
+    required:true
+   },
+
+   image:{
+    type:String,
+    required:true
+   },
+},
+{
+    timestamps:true  //created at and updated at
 });
+
+const Product = mongoose.model('Product',productSchema);
+// model() = Creates a class (model) based on the schema
+// It connects the name "Product" with the productSchema.
+// Now, you can use Product in your code to create, read, update, and delete documents in MongoDB.
+
+// This will create/use a MongoDB collection called products
+//  (note: model name is singular, MongoDB makes it plural automatically).
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import Product from "./models/product.model.js";
 
 dotenv.config(); // Must come AFTER checking path
 
 const app = express();
 
-app.get("/products",(req,res)=>{});
+app.use(express.json()); //middleware to parse incoming JSON
+
+app.post("/products",async (req,res)=>{
+  const product = req.body;
+   //user will send the data
+
+   if(!product.name || !product.price || !product.image){
+    return res.status(400).json({success:false , message:"Please provide all the fields"});
+   }
+
+   const newProduct = new Product(product);
+
+   try
+   {
+    await newProduct.save();
+   }
+   catch(error)
+   {
+    console.error("Error in creating product:",error.message);
+    res.status(500).json({success:false,message:"Server Error"});
+   }
+   
+});
 
 console.log("MONGO_URI:", process.env.MONGO_URI); // 👈 Check if it's still undefined
 
 app.listen(5000, () => {
   connectDB();
-  console.log("Server started at  http://localhost:5000 ");
+  console.log("Server started at  http://localhost:5000");
 });
